25d47ce9a70581ef448f967a49094f60f990b17f
Gian20luca <gianluca.bellafronte@gmail.com>
2021-01-27 09:56:34 +0100

    first commit

---
+++ src/Config.ts
@@ -0,0 +1,9 @@
+export const config = {
+    appId: '765221b8-fb88-4f07-a1ef-b40f3ad08d91',
+    redirectUri: 'http://localhost:3000',
+    scopes: [
+      'user.read',
+      'mailboxsettings.read',
+      'calendars.readwrite'
+    ]
+  };
